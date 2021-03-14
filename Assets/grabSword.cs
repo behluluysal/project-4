a3ad0e9ea59ebe531f7e803280832cd2a6032099
behlül uysal <mockingjaytes@gmail.com>
2021-03-15 00:51:53 +0300

    equip weapn added

--- Assets/grabSword.cs
+++ Assets/grabSword.cs
@@ -6,6 +6,8 @@ public class grabSword : MonoBehaviour
 {
     private int index; //for sword index of this exact player
     private bool flag = true;
+    [SerializeField] GameObject rhand;
+    [SerializeField] GameObject lhand;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +24,23 @@ public class grabSword : MonoBehaviour
     {
         if(other.CompareTag("endCartLocation") && flag)
         {
+            gameObject.GetComponent<Animator>().Play("Idle");
             try
             {
                 index = int.Parse(transform.name.Replace("end", ""));
                 if (CartController.swords.Count - 1 >= index)
                 {
-                    CartController.swords[index].SetActive(false);
+                    CartController.swords[index].transform.parent = rhand.transform;
+                    LeanTween.moveLocal(CartController.swords[index], new Vector3(0,0,0), 1f).setOnComplete(() => {                     });
+                    CartController.swords[index].transform.localScale = new Vector3(1,1,1);
+                    LeanTween.rotateLocal(CartController.swords[index], new Vector3(0, 0, 0),1f);
                 }
                 if (CartController.swords.Count - 1 >= index + 1)
                 {
-                    CartController.swords[index + 1].SetActive(false);
+                    CartController.swords[index+1].transform.parent = lhand.transform;
+                    LeanTween.moveLocal(CartController.swords[index+1], new Vector3(0, 0, 0), 1f).setOnComplete(() => { });
+                    CartController.swords[index+1].transform.localScale = new Vector3(1, 1, 1);
+                    LeanTween.rotateLocal(CartController.swords[index+1], new Vector3(150, 0, 0), 1f);
                 }
                 flag = false;
             }
