d9b716a372d8b56ca3fdff5aa805493caca21926
behlül uysal <mockingjaytes@gmail.com>
2021-03-14 23:17:46 +0300

    Added endgame character movements and sword pickups, but animations are not ready yet

---
+++ Assets/grabSword.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabSword : MonoBehaviour
+{
+    private int index; //for sword index of this exact player
+    private bool flag = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("endCartLocation") && flag)
+        {
+            try
+            {
+                index = int.Parse(transform.name.Replace("end", ""));
+                if (CartController.swords.Count - 1 >= index)
+                {
+                    CartController.swords[index].SetActive(false);
+                }
+                if (CartController.swords.Count - 1 >= index + 1)
+                {
+                    CartController.swords[index + 1].SetActive(false);
+                }
+                flag = false;
+            }
+            catch (System.Exception)
+            {
+                Debug.Log(transform.name);
+                throw;
+            }
+            
+        }
+    }
+}
